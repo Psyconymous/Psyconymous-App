872a704f9641ededdb0bea7e50e89fa8e9eeeef3
YQDevelops <lowyewqing@gmail.com>
2021-08-22 02:27:03 +0800

    Migrated to vite

---
+++ frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig  } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+const path = require("path")
+
+export default defineConfig({
+  server: {
+    port: 8080,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ]
+  },
+  plugins: [vue()]
+})
