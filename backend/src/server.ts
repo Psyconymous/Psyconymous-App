8f6d63222871ec83e72bd935f198705df6575442
= <spikyron@gmail.com>
2021-09-01 13:11:04 +0800

    implemented clustering for production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { Server, Socket } from 'socket.io'
 import socketHandlers from './sockets/sockets'
 import { User } from './interfaces'
 import memoryStorage from './sockets/sessionStorage'
+import clustering from './clusters/clusters'
 
 const port = process.env.PORT || 5000
 const io = new Server(app, {
@@ -22,6 +23,17 @@ io.on('connection', (socket: Socket) => {
 	socketHandlers.main(socket, io, DB, sessionDB)
 })
 
-app.listen(port, () => {
-  console.log('🚀 Server Starting At http://localhost:' + port)
-})
+if (process.env.NODE_ENV === 'production') {
+  console.log('Production Server Online')
+  clustering(() => {
+    app.listen(port, () => {
+      console.log('🚀 Server Starting At http://localhost:' + port)
+    })
+  })
+} else {
+  console.log('Development Server Online')
+  app.listen(port, () => {
+    console.log('🚀 Server Starting At http://localhost:' + port)
+  })
+}
+
